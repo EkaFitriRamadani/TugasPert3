264a742287571427fc32afa51045a3f62e51a4e4
Eka Fitri Ramadani <eka.efira@gmail.com>
2021-03-03 19:26:27 +0800

    update

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -6,21 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
 
-    public void move(View view) {
-        Intent a = new Intent(MainActivity.this, MainActivity2.class);
+    public void backToBeranda(View view) {
+        Intent a = new Intent(MainActivity2.this, MainActivity.class);
         startActivity(a);
     }
 
-    public void move1(View view) {
-        Intent a = new Intent(MainActivity.this, MainActivity3.class);
+    public void goToBiodata(View view) {
+        Intent a = new Intent(MainActivity2.this, MainActivity3.class);
         startActivity(a);
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity3.java
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -6,21 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main3);
     }
 
-    public void move(View view) {
-        Intent a = new Intent(MainActivity.this, MainActivity2.class);
+    public void backBeranda(View view) {
+        Intent a = new Intent(MainActivity3.this, MainActivity.class);
         startActivity(a);
     }
 
-    public void move1(View view) {
-        Intent a = new Intent(MainActivity.this, MainActivity3.class);
+    public void backToArticle(View view) {
+        Intent a = new Intent(MainActivity3.this, MainActivity2.class);
         startActivity(a);
     }
 }
